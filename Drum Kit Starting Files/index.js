9d8f705d1aa771532487316162ee9f2ef8f6346d
Srijan <srijannn17@gmail.com>
2025-01-22 22:26:11 +0530

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,47 @@
+var size=document.querySelectorAll(".drum").length;
+for(var i=0;i<size;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function handleClicked(){
+        var ele = this.innerHTML;
+        makeSound(ele);
+    });
+}
+
+document.addEventListener("keypress",function(eve){
+    makeSound(eve.key);
+});
+
+function makeSound(key){
+    switch (key) {
+            case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+            case 'a':
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+            break;
+            case 's':
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+            break;
+            case 'd':
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+            break;
+            case 'j':
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+            break;
+            case 'k':
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+            break;
+            case 'l':
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+            break;
+        
+        default:
+            break;
+    }
+}
